8d67b136f6fb3df5a829644f37978459591dc1f9
Annie <anniehartmann97@gmail.com>
2022-05-02 17:36:05 +0300

    Include font Roboto

--- src/App.js
+++ src/App.js
@@ -2,27 +2,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="row gx-5">
-          <div className="col-12">
-            <div className="block text-center">5</div>
-          </div>
-        </div>
-        <div className="row gy-3">
-          <div className="col-4">
-            <div className="block">5</div>
-          </div>
-          <div className="col-4">
-            <div className="block">5</div>
-          </div>
-          <div className="col-4">
-            <div className="block">5</div>
-          </div>
-        </div>
-        <div className="row gy-3">
-          <div className="col-5">
-              <div className="block">5</div>
-            </div>
-        </div>
+        
       </div>
     </>
   );
