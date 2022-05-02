9be65b47ead7efeb2e673ae0579791301657b7f1
Annie <anniehartmann97@gmail.com>
2022-05-02 20:08:47 +0300

    Create Button component and fix fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
+import Buttons from "./components/Buttons";
+import GlobalStyle from "./styles/GlobalStyles";
+
+const theme = {
+  color: {
+    white: '#ffffff',
+    black: '#211F54',
+    primary: '#0075FF',
+    grey: '#EFF0F6',
+    transparent: 'transparent'
+  }
+}
+
 function App() {
   return (
     <>
       <div className="container">
-        
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Buttons>
+            <Button>
+              Get a free quote
+            </Button>
+            <Button full={false}>
+              Explore services
+            </Button>
+          </Buttons>
+        </ThemeProvider>
       </div>
     </>
   );
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import { rem } from 'polished'
+
+const Buttons = ({children}) => {
+  return (
+    <ButtonsContainer>
+      {children}
+    </ButtonsContainer>
+  )
+}
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  gap: ${rem(28)};
+`
+
+export default Buttons
