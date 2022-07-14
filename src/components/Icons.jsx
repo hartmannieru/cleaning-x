e7bd2663f18971e7747fb705c44fe8a8c92e611b
Annie <anniehartmann97@gmail.com>
2022-07-14 22:23:11 +0300

    Fix

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -7,8 +7,14 @@ const Icons = ({ children }) => {
 };
 
 const IconsBox = styled.div`
+  width: auto;
   display: flex;
+  justify-content: center;
   gap: ${rem(100)};
+  flex-wrap: wrap;
+  @media (max-width: 783px) {
+    gap: ${rem(50)};
+  }
 `;
 
 export default Icons;
