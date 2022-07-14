95068ff57fde125ea2c675eb6784ddb90566c30a
Annie <anniehartmann97@gmail.com>
2022-07-15 01:57:13 +0300

    Fix

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  color: {
+    white: '#ffffff',
+    black: '#211F54',
+    primary: '#0075FF',
+    description: '#6E7191',
+    grey: '#EFF0F6',
+    transparent: 'transparent',
+  },
+};
+
+export default theme;
